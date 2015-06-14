60e36a4672b782e7854eb0b21f5a260782414e21
Andrew Gehman <andrew.gehman@gmail.com>
2015-06-14 12:37:08 -0400

    updated offset
    updated offset

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ $( document ).ready(function() {
 			$( 'body' ).append('<a class="back-to-top" href="#"><i class="glyphicon glyphicon-chevron-up"></i></a>');
   			
 			
-		    var offset = 600; // sets distance in pixels when back to top will appear 
+		    var offset = 400; // sets distance in pixels when back to top will appear 
 			var fadeintime = 400;  // fade in duration 
 			$(window).scroll(function() {
 				if ($(this).scrollTop() > offset) {
